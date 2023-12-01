f237db2b6a1e74a8c531accb328652fbb42e8668
MFriend <Michael.Friend@freedompay.com>
2023-12-01 12:59:34 -0500

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { line ->
+            "${line.first { it.isDigit() }}${line.last { it.isDigit() }}"
+        }.sumOf { it.toInt() }
     }
 
+    val numTable = mapOf(
+        "zero" to 0,
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
+    )
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            val (_, firstString) = it.findAnyOf(numTable.keys + "0123456789".toCharArray().map(Char::toString))
+                ?: error("Didnt find key in $it")
+            val (_, lastString) = it.findLastAnyOf(numTable.keys + "0123456789".toCharArray().map(Char::toString))
+                ?: error("Didnt find key in $it")
+            ((firstString.toIntOrNull() ?: numTable[firstString]!!) * 10) + (lastString.toIntOrNull()
+                ?: numTable[lastString]!!)
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
